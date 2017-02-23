9d9a6726c91b077a6ca15718c139f6277ffa82ea
Hajar Mokhtafa <hajarmokhtafa@gmail.com>
2017-02-23 02:17:28 +0100

    changes in various files

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
-test
+const 	express = require('express'),
+		
+		app = express(),
+
+		pg = require('pg'),
+
+		pug = require('pug'),
+
+		bodyParser = require('body-parser'),
+
+		cookieParser = require('cookie-parser'),
+
+		session = require('express-session')
+
+
+var Sequelize = require('sequelize');
+
+// creating a new database called blogapplication
+var sequelize = new Sequelize('postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blogapplication'); 
+
+app.use(express.static('static'));
+
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json()); 
+
+app.set('views', './views'); 
+app.set('view engine', 'pug');
+
+// als je dit runt dan maak die automatisch de tabel aan omdat je dit al gelinkt hebt in de var sequelize
+
+// var Message = sequelize.define('nameOfTable', {
+
+// 	title: Sequelize.STRING,
+
+// 	body: Sequelize.STRING
+
+// }); 
+
+
+// renders corresponding index.pug file
+app.get ('/', (request, response) => {
+	response.render('index');
+});
+
+// renders corresponding login.pug file
+app.get ('login', (request, response) => {
+	response.render('login');
+});
+
+// create app.post for the login page. This is because the client has to fill in something on that page
+
+
+
+
+sequelize
+	.sync()
+
+	.then(function(){
+
+		app.listen(3000, () => {
+
+			console.log('server has started');
+
+		});
+
+	})
